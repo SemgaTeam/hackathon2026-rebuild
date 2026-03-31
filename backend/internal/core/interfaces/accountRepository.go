91cc96cb07f9e866a8d056d7066c6292cfc9903c
losion <losion445@gmail.com>
2026-04-01 02:46:04 +0300

    [change] Add ctx to every mething of repository

--- backend/internal/core/interfaces/accountRepository.go
+++ backend/internal/core/interfaces/accountRepository.go
@@ -1,13 +1,17 @@
 package interfaces
 
-import "github.com/SemgaTeam/semga-stream/internal/core/entities"
+import (
+	"context"
+
+	"github.com/SemgaTeam/semga-stream/internal/core/entities"
+)
 
 type AccountRepository interface {
-	Save(account *entities.Account) (string, error)
-	FindByID(id string) (*entities.Account, error)
-	FindByUsername(username string) (*entities.Account, error)
-	FindAll() ([]entities.Account, error)
-	UpdateAccount(account *entities.Account) error
-	DeleteAccount(id string) error
-	ExistsByUsername(username string) (bool, error)
+	Save(ctx context.Context, account *entities.Account) (string, error)
+	FindByID(ctx context.Context, id string) (*entities.Account, error)
+	FindByUsername(ctx context.Context, username string) (*entities.Account, error)
+	FindAll(ctx context.Context) ([]entities.Account, error)
+	UpdateAccount(ctx context.Context, account *entities.Account) error
+	DeleteAccount(ctx context.Context, id string) error
+	ExistsByUsername(ctx context.Context, username string) (bool, error)
 }
