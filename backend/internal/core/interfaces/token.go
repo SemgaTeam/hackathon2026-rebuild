13497b915d15c4a39808badde85efe009ee9fd91
losion <losion445@gmail.com>
2026-04-03 19:15:38 +0300

    [fix] Added go context into IToken methods

--- backend/internal/core/interfaces/token.go
+++ backend/internal/core/interfaces/token.go
@@ -1,12 +1,14 @@
 package interfaces
 
 import (
+	"context"
+
 	"github.com/SemgaTeam/semga-stream/internal/core/dto"
 	"github.com/SemgaTeam/semga-stream/internal/core/entities"
 )
 
 type IToken interface {
-	GenerateTokens(account *entities.Account) (dto.Tokens, error)
-	VerifyRefresh(refToken string) (string, error)
-	GenerateAccess(account *entities.Account) (string error)
+	GenerateTokens(ctx context.Context, account *entities.Account) (dto.Tokens, error)
+	VerifyRefresh(ctx context.Context, refToken string) (string, error)
+	GenerateAccess(ctx context.Context, account *entities.Account) (string error)
 }
